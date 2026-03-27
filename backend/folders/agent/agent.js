30ae08f0d9d60ce64a92429913fac1189195bbfd
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-03-27 18:09:14 +0530

    added

--- backend/folders/agent/agent.js
+++ backend/folders/agent/agent.js
@@ -1,32 +1,74 @@
-import { client } from "../openai.js";
+////////////////////////
+//MADE WITH LLM- GPT
 
-export const runAgent = async (input) => {
-  const response = await client.chat.completions.create({
-    // model: "gpt-4.1-mini",
-    model: "openai/gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `
+// import { client } from "../openai.js";
+
+// export const runAgent = async (input) => {
+//   const response = await client.chat.completions.create({
+//     model: "gpt-4.1-mini",
+//     // model: "openai/gpt-3.5-turbo",
+//     messages: [
+//       {
+//         role: "system",
+//         content: `
+// You are an AI task manager agent.
+
+// Your job:
+// - Understand user requests
+// - Help with tasks (add, update, delete, suggest)
+// - Respond clearly and shortly
+// - If user gives a task, rephrase it cleanly
+
+// Do not give long explanations.
+// `,
+//       },
+//       { role: "user", content: input },
+//     ],
+//   });
+
+//   //   return response.choices[0].message.content;
+//   const result = response.choices[0].message.content;
+
+//   console.log("🤖 Agent Response:", result); // 👈 THIS LINE
+
+//   return result;
+// };
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//MADE WITH LLM+SDK
+
+// process.env.OPENAI_AGENTS_DISABLE_TRACING = "true";
+import "dotenv/config";
+
+import { Agent, run } from "@openai/agents";
+
+const agent = new Agent({
+  name: "TaskManager",
+  model: "gpt-4.1-mini",
+  instructions: `
 You are an AI task manager agent.
 
 Your job:
 - Understand user requests
 - Help with tasks (add, update, delete, suggest)
+-if the command comes update to the user given so only add the user given task  
 - Respond clearly and shortly
 - If user gives a task, rephrase it cleanly
 
 Do not give long explanations.
 `,
-      },
-      { role: "user", content: input },
-    ],
-  });
+});
 
-  //   return response.choices[0].message.content;
-  const result = response.choices[0].message.content;
+export const runAgent = async (input) => {
+  const response = await run(agent, input);
 
-  console.log("🤖 Agent Response:", result); // 👈 THIS LINE
+  const result = response.finalOutput;
+
+  console.log(
+    "🤖 Agent Response::::::::::::::::::::::::::::::::::::::::::::::::::::::::;;:",
+    result,
+  );
 
   return result;
 };
--- backend/folders/openai.js
+++ backend/folders/openai.js
@@ -1,11 +1,15 @@
-import OpenAI from "openai";
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env", quiet: true });
+//////////////////
+//USE THIS FILE WHEN MADE AGENT ONLY WITH LLM BY IMPORTING IT-
+// import { client } from "../openai.js";
 
-export const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  // baseURL: process.env.OPENAI_BASE_URL,
-  baseURL: "https://openrouter.ai/api/v1",
-});
+// import OpenAI from "openai";
+// import dotenv from "dotenv";
+// dotenv.config({ path: "../.env", quiet: true });
 
-// console.log("KEY:", process.env.OPENAI_API_KEY);
+// export const client = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+//   // baseURL: process.env.OPENAI_BASE_URL,
+//   baseURL: "https://openrouter.ai/api/v1",
+// });
+
+// // console.log("KEY:", process.env.OPENAI_API_KEY);
